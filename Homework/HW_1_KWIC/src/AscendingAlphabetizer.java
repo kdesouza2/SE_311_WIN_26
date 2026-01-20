2ac998c7ab9c5ef38ef46a1f9147f8b2e01b1d7c
kdesouza2 <keyra.desouza@gmail.com>
2026-01-19 20:15:02 -0500

    edit alphabetizer

--- Homework/HW_1_KWIC/src/AscendingAlphabetizer.java
+++ Homework/HW_1_KWIC/src/AscendingAlphabetizer.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AscendingAlphabetizer implements Alphabetizer {
 
-    
+    @Override
+    public List<String> sort(List<String> lines) {
+        List<String> sorted = new ArrayList<>(lines);
+        Collections.sort(sorted);
+        return sorted;
+    }
     
 }
--- Homework/HW_1_KWIC/src/DescendingAlphabetizer.java
+++ Homework/HW_1_KWIC/src/DescendingAlphabetizer.java
@@ -1,3 +1,14 @@
-public class DescendingAlphabetizer {
-    
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class DescendingAlphabetizer implements Alphabetizer {
+
+    @Override
+    public List<String> sort(List<String> lines) {
+        List<String> sorted = new ArrayList<>(lines); 
+        sorted.sort(Collections.reverseOrder());
+        return sorted;
+    }
 }
+
